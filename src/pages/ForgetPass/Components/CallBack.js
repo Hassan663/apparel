6b4c3f38e6847dbb879537fa06865376510af763
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2023-08-28 20:57:45 +0500

    forget password [ADD]

--- src/pages/ForgetPass/Components/CallBack.js
+++ src/pages/ForgetPass/Components/CallBack.js
@@ -2,14 +2,15 @@
 import React, { useState } from 'react';
 
 import { Alert } from "react-native";
-// import auth from '@react-native-firebase/auth';
+import auth from '@react-native-firebase/auth';
 
-// export const handleResetPassword = async (email) => {
-//     try {
-//         await auth().sendPasswordResetEmail(email);
-//         Alert.alert('Password Reset', 'An email has been sent to reset your password.');
-//     } catch (error) {
-//         Alert.alert(error.message)
-//         console.error('Password reset error:', error.message);
-//     }
-// };
+export const handleResetPassword = async (email,navigation) => {
+    try {
+        await auth().sendPasswordResetEmail(email);
+        Alert.alert('Password Reset', 'An email has been sent to reset your password.');
+        navigation.navigate('Login');
+    } catch (error) {
+        Alert.alert(error.message)
+        console.error('Password reset error:', error.message);
+    }
+};
